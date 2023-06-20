78704dd82ac49235898c7d62fff8b51bb5f546dd
bergben <bergben@tutanota.com>
2023-06-20 17:54:06 +0200

    refactor: remove typed contracts except used ones, do not expose anymore; remove getSafeVersion() and getTransactionReceiptStatus();

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,32 +1,11 @@
 import { Provider } from "@ethersproject/providers";
 import { Contract, Signer } from "ethers";
 import { parse } from "semver";
-import {
-  AVOCADO_AUTHORITIES_LIST_PROXY_ADDRESS,
-  AVOCADO_DEPOSIT_MANAGER_PROXY_ADDRESS,
-  AVOCADO_FACTORY_PROXY_ADDRESS,
-  AVOCADO_FORWARDER_PROXY_ADDRESS,
-  AVOCADO_GAS_ESTIMATIONS_HELPER_ADDRESS,
-  AVOCADO_SIGNERS_LIST_PROXY_ADDRESS,
-  AVOCADO_VERSIONS_REGISTRY_PROXY_ADDRESS,
-} from "./config";
-import { AvoAuthoritiesList } from "./contracts/AvoAuthoritiesList";
-import { AvoDepositManager } from "./contracts/AvoDepositManager";
-import { AvoFactory } from "./contracts/AvoFactory";
+import { AVOCADO_FORWARDER_PROXY_ADDRESS } from "./config";
 import { AvoForwarder } from "./contracts/AvoForwarder";
-import { AvoGasEstimationsHelper } from "./contracts/AvoGasEstimationsHelper";
-import { AvoSignersList } from "./contracts/AvoSignersList";
-import { AvoVersionsRegistry } from "./contracts/AvoVersionsRegistry";
 import {
-  AvoAuthoritiesList__factory,
-  AvoDepositManager__factory,
-  AvoFactory__factory,
   AvoForwarder__factory,
-  AvoGasEstimationsHelper__factory,
   AvoMultisigV3__factory,
-  AvoSignersList__factory,
-  AvoVersionsRegistry__factory,
-  AvoWalletV2__factory,
   AvoWalletV3__factory,
 } from "./contracts/factories";
 import { getRpcProvider } from "./providers";
@@ -49,51 +28,6 @@ class AvoContracts {
     contractAddress: string
   ) => chainId + contractAddress;
 
-  authoritiesList(chainId: number | string, signer?: Signer) {
-    const contractKey = this.getContractKey(
-      chainId,
-      AVOCADO_AUTHORITIES_LIST_PROXY_ADDRESS
-    );
-    if (!this.contractInstances[contractKey]) {
-      this.contractInstances[contractKey] = AvoAuthoritiesList__factory.connect(
-        AVOCADO_AUTHORITIES_LIST_PROXY_ADDRESS,
-        signer || getRpcProvider(chainId)
-      );
-    }
-
-    return this.contractInstances[chainId] as AvoAuthoritiesList;
-  }
-
-  depositManager(chainId: number | string, signer?: Signer) {
-    const contractKey = this.getContractKey(
-      chainId,
-      AVOCADO_DEPOSIT_MANAGER_PROXY_ADDRESS
-    );
-    if (!this.contractInstances[contractKey]) {
-      this.contractInstances[contractKey] = AvoDepositManager__factory.connect(
-        AVOCADO_DEPOSIT_MANAGER_PROXY_ADDRESS,
-        signer || getRpcProvider(chainId)
-      );
-    }
-
-    return this.contractInstances[chainId] as AvoDepositManager;
-  }
-
-  factory(chainId: number | string, signer?: Signer) {
-    const contractKey = this.getContractKey(
-      chainId,
-      AVOCADO_FACTORY_PROXY_ADDRESS
-    );
-    if (!this.contractInstances[contractKey]) {
-      this.contractInstances[contractKey] = AvoFactory__factory.connect(
-        AVOCADO_FACTORY_PROXY_ADDRESS,
-        signer || getRpcProvider(chainId)
-      );
-    }
-
-    return this.contractInstances[chainId] as AvoFactory;
-  }
-
   forwarder(chainId: number | string, signer?: Signer) {
     const contractKey = this.getContractKey(
       chainId,
@@ -109,59 +43,8 @@ class AvoContracts {
     return this.contractInstances[chainId] as AvoForwarder;
   }
 
-  gasEstimationsHelper(chainId: number | string, signer?: Signer) {
-    const contractKey = this.getContractKey(
-      chainId,
-      AVOCADO_GAS_ESTIMATIONS_HELPER_ADDRESS
-    );
-    if (!this.contractInstances[contractKey]) {
-      this.contractInstances[contractKey] =
-        AvoGasEstimationsHelper__factory.connect(
-          AVOCADO_GAS_ESTIMATIONS_HELPER_ADDRESS,
-          signer || getRpcProvider(chainId)
-        );
-    }
-
-    return this.contractInstances[chainId] as AvoGasEstimationsHelper;
-  }
-
   multisigV3(address: string, signer: Signer | Provider) {
     return AvoMultisigV3__factory.connect(address, signer);
-  }
-
-  signersList(chainId: number | string, signer?: Signer) {
-    const contractKey = this.getContractKey(
-      chainId,
-      AVOCADO_SIGNERS_LIST_PROXY_ADDRESS
-    );
-    if (!this.contractInstances[contractKey]) {
-      this.contractInstances[contractKey] = AvoSignersList__factory.connect(
-        AVOCADO_SIGNERS_LIST_PROXY_ADDRESS,
-        signer || getRpcProvider(chainId)
-      );
-    }
-
-    return this.contractInstances[chainId] as AvoSignersList;
-  }
-
-  versionsRegistry(chainId: number | string, signer?: Signer) {
-    const contractKey = this.getContractKey(
-      chainId,
-      AVOCADO_VERSIONS_REGISTRY_PROXY_ADDRESS
-    );
-    if (!this.contractInstances[contractKey]) {
-      this.contractInstances[contractKey] =
-        AvoVersionsRegistry__factory.connect(
-          AVOCADO_VERSIONS_REGISTRY_PROXY_ADDRESS,
-          signer || getRpcProvider(chainId)
-        );
-    }
-
-    return this.contractInstances[chainId] as AvoVersionsRegistry;
-  }
-
-  safeV2(address: string, signer: Signer | Provider) {
-    return AvoWalletV2__factory.connect(address, signer);
   }
 
   safeV3(address: string, signer: Signer | Provider) {
--- src/signer.ts
+++ src/signer.ts
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-import { TransactionResponse, Provider, TransactionRequest, TransactionReceipt } from '@ethersproject/abstract-provider'
+import { TransactionResponse, Provider, TransactionRequest } from '@ethersproject/abstract-provider'
 import { Signer, TypedDataDomain, TypedDataField, TypedDataSigner } from '@ethersproject/abstract-signer'
 import { Deferrable } from '@ethersproject/properties'
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
@@ -11,7 +11,7 @@ import { parse } from 'semver';
 import { AVOCADO_CHAIN_ID } from './config'
 import { signTypedData } from './utils/signTypedData'
 import { AvoCoreStructs, AvoForwarder, IAvoWalletV1, IAvoWalletV2 } from './contracts/AvoForwarder'
-import { AvoForwarder__factory, AvoWalletV3__factory } from './contracts/factories'
+import { AvoWalletV3__factory } from './contracts/factories'
 import { AvoWalletV3 } from './contracts/AvoWalletV3'
 
 export interface SignatureOption {
@@ -375,29 +375,6 @@ class AvoSigner extends Signer implements TypedDataSigner {
     )
   }
 
-  async getTransactionReceiptStatus(txReceipt: TransactionReceipt) {
-    const iface = AvoForwarder__factory.createInterface();
-
-    let executeFailedLog = txReceipt.logs.find(
-      (l) =>
-        l.topics[0] ===
-        "0x420a063f564b9a9600709d5d1e36535a3edcb1eede9c459720dd684b6d76f75e"
-    );
-
-    if(!executeFailedLog) {
-      return {success: true , error: undefined}
-    }
-    
-    let error;
-    try {
-      error = iface.parseLog(executeFailedLog).args[4];
-    } catch (error) {
-      console.warn("avocado-sdk: Parsing error log failed");
-    }
-
-    return {success: false, error}
-  }
-
   signMessage(_message: any): Promise<string> {
     throw new Error('Method not implemented.')
   }
@@ -639,27 +616,6 @@ export function createSafe(signer: Signer, provider = signer.provider, ownerAddr
      */
     async getSafeNonce(chainId: number | string) {
       return await avoSigner.getSafeNonce(Number(chainId))
-    },
-
-    /**
-     * Get the safe version of the current AvoSigner instance
-     * 
-     * @returns current avoSigner instance version
-     */
-    async getSafeVersion(chainId: number | string) {
-      return await avoContracts.safeVersion(chainId, await avoSigner.getAddress());
-    },
-
-    /**
-     * Get the status of a transaction receipt with success flag and error message
-     * 
-     * @param txReceipt the transaction receipt obtained e.g. by using `sendTransaction()` with returned `tx.wait()`
-     * @returns success flag and if false, also returns the error message as emitted by the `ExecuteFailed` event
-     */
-    async getTransactionReceiptStatus(txReceipt: TransactionReceipt): Promise<{ 
-      success: boolean; error: string  | undefined
-    }> {
-      return await avoSigner.getTransactionReceiptStatus(txReceipt);
     },
   }
 }
