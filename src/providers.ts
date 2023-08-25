6652764f4badc4d3a277db9a19781c1f74da6757
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2023-08-25 13:16:52 +0300

    remove ankr

--- src/providers.ts
+++ src/providers.ts
@@ -2,18 +2,18 @@ import { StaticJsonRpcProvider } from '@ethersproject/providers'
 import { AVOCADO_CHAIN_ID, AVOCADO_RPC } from './config'
 
 export const RPC_URLS: { [chainId: number]: string } = {
-    1: 'https://rpc.ankr.com/eth',
-    137: 'https://rpc.ankr.com/polygon',
-    43114: 'https://rpc.ankr.com/avalanche',
-    250: 'https://rpc.ankr.com/fantom',
-    10: 'https://rpc.ankr.com/optimism',
+    1: 'https://eth.drpc.org',
+    137: 'https://polygon-rpc.com',
+    43114: 'https://api.avax.network/ext/bc/C/rpc',
+    250: 'https://rpc.ftm.tools',
+    10: 'https://mainnet.optimism.io',
     42161: 'https://arb1.arbitrum.io/rpc',
     [AVOCADO_CHAIN_ID]: AVOCADO_RPC,
-    100: 'https://rpc.ankr.com/gnosis',
-    56: 'https://rpc.ankr.com/bsc',
-    1101 : 'https://rpc.ankr.com/polygon_zkevm',
+    100: 'https://rpc.gnosischain.com',
+    56: 'https://bsc-dataseed.binance.org',
+    1101 : 'https://zkevm-rpc.com',
     1313161554: "https://mainnet.aurora.dev",
-    8453: "https://rpc.ankr.com/base",
+    8453: "https://developer-access-mainnet.base.org",
 }
 
 const rpcInstances: Record<string, StaticJsonRpcProvider> = {}
